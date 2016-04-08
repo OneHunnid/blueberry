a7cc7c7588e301806b1ba513b00c081676dee01f
onehunnid <climbrick1989@gmail.com>
2016-04-07 21:06:00 -0400

    Update merge conflict

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,60 @@ var gulp = require('gulp');
 
 var gutil = require('gulp-util');
 var connect = require('gulp-connect');
-var babel = require('gulp-babel');
+// var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
+// var concat = require('gulp-concat');
 var less = require('gulp-less');
 var lessPluginCleanCSS = require('less-plugin-clean-css');
 var lessPluginAutoPrefix = require('less-plugin-autoprefix');
 
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash.assign');
+var concat = require('gulp-concat-sourcemap');
+var exec = require('child_process').exec;
+var babel = require('babelify');
+var watchify = require('watchify');
+var browserify = require('browserify');
+
 cleancss = new lessPluginCleanCSS({ advanced: true });
 autoprefix = new lessPluginAutoPrefix({ browser: ["last 2 versions"] });
 
+// Add custom browserify options here
+var customOpts = {
+	entries: ['./app/js/app.js'],
+	debug: true;
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+gulp.task('js', bundle);
+
+// Add transformations here
+b.transform("babelify", {presets: ["es2015", "react"]})
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+
+function bundle() {
+		return b.bundle()
+		.on('error', gutil.log.bind(gutil, 'Browserify Error')) // log errors if they happen
+		.pipe(source('build.js'))
+		.pipe(sourcemaps.write('./')) // write .map file
+		.pipe(gulp.dest('./app/js'));
+}
+
+// ========================================================================
+
 // Scripts Tasks - Compiles ECMAScript 6 and minifies
 gulp.task('scripts', function() {
-	gulp.src('app/js/*.js')
-			.pipe(babel({
-				presets: ['es2015']
-			}))
-			.on('error', gutil.log)
-			.pipe(uglify())
-			.pipe(concat('build.js'))
-			.pipe(gulp.dest('app/js'))
+	// gulp.src('app/js/*.js')
+	// 		.pipe(babel({
+	// 			presets: ['es2015']
+	// 		}))
+	// 		.on('error', gutil.log)
+	// 		.pipe(uglify())
+	// 		.pipe(gulp.dest('build/js'))
 });
 
 // Styles Tasks - Turns LESS to CSS, autoprefixes and minifies
@@ -31,9 +65,8 @@ gulp.task('less', function() {
 			.pipe(less({
 				plugins: [autoprefix, cleancss]
 			}))
-			.pipe(concat('build.css'))
-			.pipe(gulp.dest('app/styles'))
-			.pipe(connect.reload()); 
+			.pipe(concat('main.css'))
+			.pipe(gulp.dest('build/styles'))
 });
 
 // HTML Task - Reloads HTML
@@ -46,7 +79,7 @@ gulp.task('html', function () {
 gulp.task('watch', function() {
 	gulp.watch('app/js/*.js', ['scripts']);
 	gulp.watch('app/styles/*.less', ['less']);
-	gulp.watch('app/*.html', ['html']);
+	gulp.watch(['app/*.html'], ['html']);
 });
 
 // Connect Task - Starts localhost:8080
