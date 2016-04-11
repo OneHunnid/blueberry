b60b113f043196dd8e6f46eff6c9129184a17d78
Chris Limbrick <climbrick1989gmail.com@>
2016-04-10 21:18:00 -0400

    updating the compiledJS for new gulp-load-plugins module

--- gulp/gulp-tasks/compileStyles.js
+++ gulp/gulp-tasks/compileStyles.js
@@ -1,11 +1,11 @@
-module.exports = function(gulp, plugins, pluginsUtil, pluginsLess, pluginsConcat, pluginsConnect) {
+module.exports = function(gulp, plugins, pluginsUtil, pluginsLess, pluginsConcatSourcemap, pluginsConnect) {
 		return function() {
 			gulp.src(['app/styles/normalize.less', 'app/styles/main.less'])
 					.on('error', plugins.util.log)
 					.pipe(plugins.less({
 						plugins: [autoprefix, cleancss]
 					}))
-					.pipe(plugins.concat('build.css'))
+					.pipe(plugins.concatSourcemap('build.css'))
 					.pipe(gulp.dest('app/styles'))
 					.pipe(plugins.connect.reload());
 		};
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,5 @@
 var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')({
-		name: {
-			'gulp-concat-sourcemap' : 'concat'
-		}
-});
+var plugins = require('gulp-load-plugins')();
 
 var lessPluginCleanCSS = require('less-plugin-clean-css');
 var lessPluginAutoPrefix = require('less-plugin-autoprefix');
@@ -20,34 +16,34 @@ autoprefix = new lessPluginAutoPrefix({ browser: ["last 2 versions"] });
 
 
 // Add custom browserify options here
-var customOpts = {
-	entries: ['./app/js/app.js'],
-	debug: true
-};
-var opts = assign({}, watchify.args, customOpts);
-var b = watchify(browserify(opts));
-
-// Add transformations here
-b.transform("babelify", {presets: ["es2015", "react"]})
-b.on('update', bundle); // on any dep update, runs the bundler
-b.on('log', plugins.util.log); // output build logs to terminal
-
-function bundle() {
-		return b.bundle()
-		.on('error', plugins.util.log.bind(plugins.util, 'Browserify Error')) // log errors if they happen
-		.pipe(source('build.js'))
-		.pipe(plugins.sourcemaps.write('./')) // write .map file
-		.pipe(gulp.dest('./app/js'))
-		.pipe(plugins.connect.reload());
-}
+// var customOpts = {
+// 	entries: ['./app/js/app.js'],
+// 	debug: true
+// };
+// var opts = assign({}, watchify.args, customOpts);
+// var b = watchify(browserify(opts));
+//
+// // Add transformations here
+// b.transform("babelify", {presets: ["es2015", "react"]})
+// b.on('update', bundle); // on any dep update, runs the bundler
+// b.on('log', plugins.util.log); // output build logs to terminal
+//
+// function bundle() {
+// 		return b.bundle()
+// 		.on('error', plugins.util.log.bind(plugins.util, 'Browserify Error')) // log errors if they happen
+// 		.pipe(source('build.js'))
+// 		.pipe(plugins.sourcemaps.write('./')) // write .map file
+// 		.pipe(gulp.dest('./app/js'))
+// 		.pipe(plugins.connect.reload());
+// }
 
 
 
 // This 'js' task needs fixin' -- compileJS does not refresh the DOM when changes are made
-// gulp.task('js', require('./gulp-tasks/compileJS')(gulp, plugins, gutil, source, plugins.sourcemaps, assign, concat, exec, babel, watchify, browserify, plugins.connect));
+gulp.task('js', require('./gulp/gulp-tasks/compileJS')(gulp, plugins, plugins.util, source, plugins.sourcemaps, assign, plugins.concatSourcemap, exec, babel, watchify, browserify, plugins.connect));
 
-gulp.task('js', bundle);
-gulp.task('plugins.less', require('./gulp/gulp-tasks/compileStyles')(gulp, plugins, plugins.util, plugins.less, plugins.concat, plugins.connect));
+// gulp.task('js', bundle);
+gulp.task('plugins.less', require('./gulp/gulp-tasks/compileStyles')(gulp, plugins, plugins.util, plugins.less, plugins.concatSourcemap, plugins.connect));
 gulp.task('html', require('./gulp/gulp-tasks/reloadHTML')(gulp, plugins, plugins.connect));
 gulp.task('plugins.connect', require('./gulp/gulp-tasks/webserver')(gulp, plugins, plugins.connect));
 
