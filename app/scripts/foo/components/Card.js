d7bf73039c59c004718fc35ad386877065a6efec
climbrick <climbrick@squarespace.com>
2016-07-14 15:39:27 -0400

    Adding enter name intro

--- app/scripts/foo/components/Card.js
+++ app/scripts/foo/components/Card.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 class Card extends React.Component {
   render() {
-    console.log(this.props.info.name)
+
+    if (this.props.info.name === undefined) {
+      return <h3>Please enter a name</h3>
+    }
+
     return (
       <div>
         <h3>Hello, {this.props.info.name}!</h3>
       </div>
-    );
+    )
   }
-
 }
 export default Card;
